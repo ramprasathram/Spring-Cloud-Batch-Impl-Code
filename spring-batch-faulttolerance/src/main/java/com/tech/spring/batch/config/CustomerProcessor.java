8c572a44e638bf642ba621f5787fbcbb48a30407
ramprasathram <ramprasath.ramesh@gmail.com>
2024-03-22 11:50:35 +0530

    Add files via upload

---
+++ spring-batch-faulttolerance/src/main/java/com/tech/spring/batch/config/CustomerProcessor.java
@@ -0,0 +1,15 @@
+package com.tech.spring.batch.config;
+
+import com.tech.spring.batch.entity.Customer;
+import org.springframework.batch.item.ItemProcessor;
+
+public class CustomerProcessor implements ItemProcessor<Customer, Customer> {
+    @Override
+    public Customer process(Customer customer) {
+        int age = Integer.parseInt(customer.getAge());//vhjkdfh38497infdhg
+        if (age >= 18) {
+            return customer;
+        }
+        return null;
+    }
+}
---
+++ spring-batch-faulttolerance/src/main/java/com/tech/spring/batch/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.tech.spring.batch.repository;
+
+import com.tech.spring.batch.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+}
